a6fbc1a33223417f061f18d4e0baf8a1e507fbd9
neeshu <neeshuhaldaur@gmail.com>
2024-12-17 21:15:02 +0530

    admin

--- src/app/common/Header.js
+++ src/app/common/Header.js
@@ -10,6 +10,7 @@ import Sidemenu from '../Menu/Sidemenu';
 
 export default function Header() {
     let [menu,setmenu]=useState(true)
+    let [admin,setadmin]=useState(true)
     let [header, setheader] = useContext(HeaderContext)
     const [ifSearch, setIfSearch] = useState('block');
     const pathname = usePathname();
@@ -44,7 +45,7 @@ export default function Header() {
                         </div>
                     </div>
                     <div className='  lg:flex '>
-                        <CiUser className='text-2xl text-orange-600'  />
+                        <CiUser className='text-2xl text-orange-600 cursor-pointer'  onClick={() => setadmin(!admin)} />
                     </div>
                 </nav>
                 <div className={`relative w-[80%]  ${header} lg:hidden block m-auto`}>
@@ -52,6 +53,7 @@ export default function Header() {
                     <input type='text' placeholder='Search Hospital ' className='ps-[35px] rounded-md h-[30px] w-[100%]' />
                 </div>
             </header>
+            {/* side menu */}
             <section>
                 <div className={`bg-[rgba(56,56,55,0.9)] w-[100%]  fixed z-20 ${menu ? `translate-x-[-100%]` : `translate-x-[0%]`}`}>
                     <div className='bg-[#fcfdfc] w-[85%]  h-[110vh] lg:mt-[-53px] mt-[-90px] relative '>
@@ -68,7 +70,21 @@ export default function Header() {
                     </div>
                 </div>
             </section>
-           
+           {/* admin user */}
+           <section>
+                <div className={`border bg-[rgba(56,56,55,0.9)] w-[100%] top-[-50px] h-[110vh] pt-[10%] z-40 fixed  ${admin ? `translate-x-[-100%]` : `translate-x-[0%]`}`}>
+                    <div className='bg-[#fcfdfc] w-[50%] rounded-lg m-auto  h-[50vh] lg:mt-[-53px] relative '>
+                        <p className={`fixed right-5 top-[-80px] pt-[10%]  text-[70px] font-bold text-[#8f6fdb] cursor-pointer  rounded-lg `}  onClick={() => setadmin(!admin)}>&times;</p>
+                        <p className='bg-[#788b76] text-white font-[500] p-0 h-[100px] text-center text-[30px] leading-[100px] rounded-lg'>Login</p>
+                        <ul className='text-[#788b76] p-5 flex flex-col gap-3 text-[30px]'>
+                            <li>Login with Google</li>
+
+                          
+                        </ul>
+
+                    </div>
+                </div>
+            </section>
 
         </>
     )
